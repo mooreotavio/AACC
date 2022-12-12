919e7b2417ec2f04c4201a3c3751aa4c52e45f92
mooreotavio <otaviomoore@gmail.com>
2022-12-12 20:01:09 -0300

    fixed index path, html

--- AACC/urls.py
+++ AACC/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from dashboard.views import IndexView
+
 urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view())
 ]
